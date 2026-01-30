478cc07b89f2e80f2cd554577a9f31883524782c
Raghad <raghadahmad9095@gmail.com>
2026-01-30 21:51:57 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,7 @@
+name = input("Task name: ")
+importance = input("Importance (1–5): ")
+time = input("Time wasted (minutes): ")
+
+print("Task: " + name)
+print("Importance: " + importance)
+print("Time wasted: " + time)
